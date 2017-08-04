b2b1c679b3188c3ec031be04e8759ef2e8de5428
Santosh Mohapatra <santoshdms2003@gmail.com>
2017-08-04 18:06:39 +0530

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-/*document.write('<scr'+'ipt type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" ></scr'+'ipt>');*/
+/*document.write('<scr'+'ipt type=\"text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" ></scr'+'ipt>');*/
 document.write('<scr'+'ipt type="text/javascript" src="test.js" ></scr'+'ipt>');
 document.write('<scr'+'ipt type="text/javascript" src="test1.js" ></scr'+'ipt>');
 alert('Executed the index.js');
-
-
