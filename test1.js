64d38b35129b541df4319066cfe9edacf53bcb69
Santosh Mohapatra <santoshdms2003@gmail.com>
2017-08-04 15:32:14 +0530

    Updates

--- test1.js
+++ test1.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  alert('Welcome to AMP page');
  $("#btnsubmit").click(function(){
-        alert("hi hr u");
+         $("#output").html("<b>Hello world!</b>");
     });
 });
