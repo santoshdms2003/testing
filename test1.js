256c0b959ac4c37c0770011caabae1d508282c5c
Santosh Mohapatra <santoshdms2003@gmail.com>
2017-08-07 13:50:01 +0530

    Updates

--- test1.js
+++ test1.js
@@ -1,7 +1,7 @@
-window.parent.$(document).ready(function(){
+$(document).ready(function(){
 	
- window.parent.$("#btnsubmit").click(function(){
-var result = "<b>Hello! </b>" + window.parent.$("#txtname").val();
-      window.parent.$("#output").html(result);
+ $("#btnsubmit").click(function(){
+var result = "<b>Hello! </b>" + $("#txtname").val();
+      $("#output").html(result);
     });
 });
